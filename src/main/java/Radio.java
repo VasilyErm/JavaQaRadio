7a5390928488efea694a85013b153ee53aa57fc9
Vasily Ermolaev <vasily.erm@gmail.com>
2023-04-04 22:16:37 +0300

    Add lombok

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,21 +1,18 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
     private int radioStation;
     private int currentVolume;
-    private int maxStation;
-    private int minStation;
-    private int maxVolume;
-    private int minVolume;
-
-    public Radio() {
-        this.maxStation = 9;
-        this.minStation = 0;
-        this.maxVolume = 100;
-        this.minVolume = 0;
-    }
-
-    public Radio(int maxStationsCount) {
-        this.maxStation = maxStationsCount - 1;
-    }
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int maxVolume = 100;
+    private int minVolume = 0;
 
     public void nextStation() {
         if (radioStation != maxStation) {
@@ -24,16 +21,13 @@ public class Radio {
             radioStation = minStation;
         }
     }
-
     public void previousStation() {
         if (radioStation != minStation) {
             radioStation--;
         } else {
             radioStation = maxStation;
         }
-    }
-
-    public void nextVolume() {
+    } public void nextVolume() {
         if (currentVolume != maxVolume) {
             currentVolume++;
         } else {
@@ -77,3 +71,81 @@ public class Radio {
         this.currentVolume = currentVolume;
     }
 }
+
+
+
+
+
+
+//
+//    public Radio() {
+//        this.maxStation = 9;
+//        this.minStation = 0;
+//        this.maxVolume = 100;
+//        this.minVolume = 0;
+//    }
+//
+//    public Radio(int maxStationsCount) {
+//        this.maxStation = maxStationsCount - 1;
+//    }
+//
+//    public void nextStation() {
+//        if (radioStation != maxStation) {
+//            radioStation++;
+//        } else {
+//            radioStation = minStation;
+//        }
+//    }
+//
+//    public void previousStation() {
+//        if (radioStation != minStation) {
+//            radioStation--;
+//        } else {
+//            radioStation = maxStation;
+//        }
+//    }
+//
+//    public void nextVolume() {
+//        if (currentVolume != maxVolume) {
+//            currentVolume++;
+//        } else {
+//            currentVolume = minVolume;
+//        }
+//    }
+//
+//    public void turnDownVolume() {
+//        if (currentVolume != minVolume) {
+//            currentVolume--;
+//        } else {
+//            currentVolume = maxVolume;
+//        }
+//    }
+//
+//    public int getRadioStation() {
+//        return radioStation;
+//    }
+//
+//    public void setRadioStation(int radioStation) {
+//        if (radioStation < minStation) {
+//            return;
+//        }
+//        if (radioStation > maxStation) {
+//            return;
+//        }
+//        this.radioStation = radioStation;
+//    }
+//
+//    public int getCurrentVolume() {
+//        return currentVolume;
+//    }
+//
+//    public void setCurrentVolume(int currentVolume) {
+//        if (currentVolume < minVolume) {
+//            return;
+//        }
+//        if (currentVolume > maxVolume) {
+//            return;
+//        }
+//        this.currentVolume = currentVolume;
+//    }
+//}
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -6,28 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class RadioTest {
 
     @Test
-    public void maxStationCount() {
-        Radio radio = new Radio(10);
-        radio.setRadioStation(9);
-
-        int expected = 9;
-        int actual = radio.getRadioStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void maxStationCount2() {
-        Radio radio = new Radio(20);
-        radio.setRadioStation(19);
-
-        int expected = 19;
-        int actual = radio.getRadioStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
     public void shouldSetRadioStation() {
         Radio radio = new Radio();
         radio.setRadioStation(5);
@@ -346,3 +324,27 @@ class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 }
+
+
+
+//@Test
+//    public void maxStationCount() {
+//        Radio radio = new Radio(10);
+//        radio.setRadioStation(9);
+//
+//        int expected = 9;
+//        int actual = radio.getRadioStation();
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void maxStationCount2() {
+//        Radio radio = new Radio(20);
+//        radio.setRadioStation(19);
+//
+//        int expected = 19;
+//        int actual = radio.getRadioStation();
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
